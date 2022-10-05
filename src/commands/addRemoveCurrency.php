48021bd5ef69ff21ee4cd95da868064062497517
Алексей Кулагин <a.kulagin@modimio.ru>
2022-10-05 12:24:04 +0300

    fix

---
+++ src/commands/addRemoveCurrency.php
@@ -0,0 +1,20 @@
+<?php
+
+require dirname(__DIR__, 2) . '/vendor/autoload.php';
+
+use testTask\services\AddRemoveCurrency;
+
+$command = readline('Choose command: add currensy (enter 1), remove currency (enter 2)');
+
+if ($command != 1 && $command != 2) {
+    echo 'Command mast be 1 or 2' . PHP_EOL;
+    exit(1);
+}
+
+$currensy = readline('Enter RUB or EUR or USD');
+
+$service = new AddRemoveCurrency();
+
+if ($command == 1) {
+    $service->addCurrency($currensy);
+}
